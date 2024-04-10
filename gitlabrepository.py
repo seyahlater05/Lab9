e773f68ec1963d0b2ee7b33078a2e84951cd3c09
orangeinkk <sam513h@gmail.com>
2024-04-10 19:16:06 -0400

    decoder

--- gitlabrepository.py
+++ gitlabrepository.py
@@ -7,6 +7,16 @@ def encode(key):
             encoded_key += f'{int(i)-7}'
     return encoded_key
 
+def decode(key):
+    decoded_key = ''
+    for i in key:
+        if int(i) > 3:
+            decoded_key += f'{int(i)-3}'
+        else:
+            decoded_key += f'{int(i)+7}'
+    return decoded_key
+
+
 
 if __name__ == '__main__':
     while 1:
@@ -15,3 +25,8 @@ if __name__ == '__main__':
         if choice == '1':
             key = input('Please enter your password to encode: ')
             encoded_key = encode(key)
+            print("Your password has been encoded and stored!\n")
+        if choice == '2':
+            print(f"The encoded password is {encoded_key} and the original password is {decode(encoded_key)}\n")
+        if choice == '3':
+            break
